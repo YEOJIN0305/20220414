c0abe0c1be40bb550a4653bfee7ee5ac67b0901b
Choiyeojin <choiyj0303@naver.com>
2022-03-29 03:49:29 +0900

    2022_03_29

---
+++ src/main/java/Date3_29_1.java
@@ -0,0 +1,9 @@
+public class Date3_29_1 {
+    public static void main(String[] args) {//for문 구구단 - 3단 부터 8단까지
+        for(int i = 3; i < 9; i++) {
+            for(int j = 1; j < 10; j++) {
+                System.out.println(i + "x" + j + "=" + (i * j));
+            }
+        }
+    }
+}
