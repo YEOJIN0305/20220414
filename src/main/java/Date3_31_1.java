e1540c15944306825e50bd83d31d8c3dca8ca426
Choiyeojin <choiyj0303@naver.com>
2022-04-01 05:41:22 +0900

    2022_04_01

---
+++ src/main/java/Date3_31_1.java
@@ -0,0 +1,17 @@
+public class Date3_31_1 {//삼각형 별찍기
+    public static void main(String[] args) {
+        int inc = 3, dec = 3;
+        for(int i = 1; i <= 3; i++) {
+            for(int j = 1; j <= inc; j++) {
+                if(j < dec) {
+                    System.out.print(" ");
+                } else {
+                    System.out.print("*");
+                }
+            }
+        }
+        System.out.println();
+        inc++;
+        dec--;
+    }
+}
