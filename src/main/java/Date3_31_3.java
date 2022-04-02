2eae582472a2fc1f3fc4ae26207e9227bba9efcb
Choiyeojin <choiyj0303@naver.com>
2022-04-03 05:35:06 +0900

    2022_04_03

---
+++ src/main/java/Date3_31_3.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Date3_31_3 {//단을 입력받는 구구단
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        System.out.print("몇 단 : ");
+        int dan = s.nextInt();
+        System.out.println(dan + "단");
+        for(int i = 1; i <= 9; i++) {
+            System.out.println(dan + "x" + i + "=" + (dan * i));
+        }
+
+    }
+}
