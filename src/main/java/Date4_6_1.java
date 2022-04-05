0537585e2844ccfa80976600b6ccc88e2afd1541
Choiyeojin <choiyj0303@naver.com>
2022-04-05 22:32:07 +0900

    2022_04_06

---
+++ src/main/java/Date4_6_1.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Date4_6_1 {//윤년 구하기/윤년일때 1, 아닐 때 0을 출력
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+
+        System.out.println("연도를 입력하시오 : ");
+        int a = s.nextInt();
+
+        if(a%4==0) {
+            if(a%400==0) {
+                System.out.println("1");
+            }else if(a%100==0){
+                System.out.println("0");
+            }
+        }
+    }
+}
