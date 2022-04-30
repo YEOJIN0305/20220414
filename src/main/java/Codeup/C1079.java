f160e577100bf3d698376c73f36ed7e60b1e62f6
Choiyeojin <choiyj0303@naver.com>
2022-05-01 00:47:36 +0900

    2022_05_01

---
+++ src/main/java/Codeup/C1079.java
@@ -0,0 +1,16 @@
+package Codeup;
+
+import java.util.Scanner;
+
+public class C1079 {
+    public static void main(String[] args) {
+        String f ="f";
+
+        Scanner s = new Scanner(System.in);
+        String a = s.nextLine();
+
+        while(a == f) {
+            System.out.println("a");
+        }
+    }
+}
