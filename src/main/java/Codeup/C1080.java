7df51d6465885be957c3efbaabe357da3a96187c
Choiyeojin <choiyj0303@naver.com>
2022-05-10 23:11:38 +0900

    2022_05_11

---
+++ src/main/java/Codeup/C1080.java
@@ -0,0 +1,15 @@
+/*
+import java.util.Scanner;
+
+public class C1080 {
+    public static void main(String[] args) {
+
+        Scanner s = new Scanner(System.in);
+        int a = s.nextInt();
+
+        for (int i=0; i<a; i+=1){
+            if (i<=a)
+        }
+    }
+}
+*/
