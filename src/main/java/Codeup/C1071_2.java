e881bd651cbcf2698dc070ea580c96b8773546d0
Choiyeojin <choiyj0303@naver.com>
2022-05-16 23:19:19 +0900

    2022_05_16

---
+++ src/main/java/Codeup/C1071_2.java
@@ -0,0 +1,20 @@
+package Codeup;
+
+import java.util.Scanner;
+
+public class C1071_2 {
+    public static void main(String[] args) {
+
+        Scanner s = new Scanner(System.in);
+
+        while(true){
+            int a = s.nextInt();
+
+            if(a ==0){
+                break;
+
+            }
+            System.out.println(a);
+        }
+    }
+}
