dd27e7827a440a24ecb125649e9f652b3ebaad39
Choiyeojin <choiyj0303@naver.com>
2022-06-22 00:06:04 +0900

    2022_06_22

---
+++ src/main/java/Test/D0518_1.java
@@ -0,0 +1,19 @@
+package Test;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class D0518_1 {
+    public static void main(String[] args) {
+
+        List<Map<String, String>> list = new ArrayList<>();
+
+        List<Map<String, String>> list2 = new ArrayList<>();
+
+        Map<String, String> map = new HashMap<>();
+        map.put("name", "이협건");
+        map.put("dept", "데이터분석");
+    }
+}
