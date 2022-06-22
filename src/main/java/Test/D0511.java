b71676b76e276b4655d49d8036ea4214cfa7a75a
Choiyeojin <choiyj0303@naver.com>
2022-06-23 00:12:18 +0900

    2022_06_23

---
+++ src/main/java/Test/D0511.java
@@ -0,0 +1,20 @@
+package Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class D0511 {
+        public static void main(String[] args) {
+
+                List<String> pList =  new ArrayList<>();
+
+                pList.add("홍길동");
+                pList.add("이협건");
+                pList.add("고길동");
+                pList.add("둘리");
+
+
+        }
+
+
+}
