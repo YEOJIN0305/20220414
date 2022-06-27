563ab54f690c8337547623504330d9e02b9fdcdb
Choiyeojin <choiyj0303@naver.com>
2022-06-27 19:53:20 +0900

    2022_06_27

---
+++ src/main/java/Test/practice.java
@@ -0,0 +1,11 @@
+package Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class practice {
+    public static void main(String[] args) {
+
+        List<String> pList = new ArrayList<>();
+    }
+}
